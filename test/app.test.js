1e666250efee742f2976432f4e88a9f43ec2b5bd
dmytro.gusev <dmytro.gusev@oracle.com>
2025-02-22 20:30:08 +0200

    jest tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,10 @@
-const mathAdd = (a, b) => a + b;
+const mathAdd = (a, b) => {
+  if (a === undefined || b === undefined) {
+    throw new Error('All arguments should exists');
+  }
+
+  return a + b;
+};
 
 describe('2 + 2 = 4', () => {
   it('should be 4', () => {
@@ -6,5 +12,30 @@ describe('2 + 2 = 4', () => {
 
     // eslint-disable-next-line no-undef
     expect(r).toEqual(4);
+  });
+
+  it('should be 2', () => {
+    const r = mathAdd(0, 2);
+
+    // eslint-disable-next-line no-undef
+    expect(r).toEqual(2);
+  });
+
+  it('should be Error', () => {
+    const r = () => {
+      mathAdd(2);
+    };
+
+    // eslint-disable-next-line no-undef
+    expect(r).toThrow();
+  });
+
+  it('should be Error2', () => {
+    const r = () => {
+      mathAdd(undefined, 2);
+    };
+
+    // eslint-disable-next-line no-undef
+    expect(r).toThrow();
   });
 });
