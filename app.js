6b0bc927e6376aff17f45a5d18a0b30e28138f8d
dmytro.gusev <dmytro.gusev@oracle.com>
2025-02-24 13:59:52 +0200

    broke lint

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ if (secureTransfer) numberOfProxies = 1; else numberOfProxies = 0;
  */
 const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
+
+
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
 
